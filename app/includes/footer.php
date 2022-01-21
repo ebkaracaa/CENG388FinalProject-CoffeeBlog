6e153fb545e5c9db17ea61f0b39c691fce3280ab
Emre Baran KARACA <50445704+ebkaracaa@users.noreply.github.com>
2022-01-21 22:21:53 +0300

    Update footer.php

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -20,40 +20,7 @@
         </div>
       </div>
 
-<!--       <div class="footer-section links">
-        <h2>Quick Links</h2>
-        <br>
-        <ul>
-          <a href="#">
-            <li>Events</li>
-          </a>
-          <a href="#">
-            <li>Team</li>
-          </a>
-          <a href="#">
-            <li>Mentores</li>
-          </a>
-          <a href="#">
-            <li>Gallery</li>
-          </a>
-          <a href="#">
-            <li>Terms and Conditions</li>
-          </a>
-        </ul>
-      </div> -->
 
-<!--       <div class="footer-section contact-form">
-        <h2>Contact us</h2>
-        <br>
-        <form action="index.html" method="post">
-          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
-          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
-          <button type="submit" class="btn btn-big contact-btn">
-            <i class="fas fa-envelope"></i>
-            Send
-          </button>
-        </form>
-      </div> -->
 
     </div>
 
