237afd0c79cc068f4a92ac7da122d6d99671dc88
Emre Baran KARACA <50445704+ebkaracaa@users.noreply.github.com>
2022-01-21 20:46:28 +0300

    Add files via upload
    First Release

---
+++ app/includes/footer.php
@@ -0,0 +1,64 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <div style="text-align:center">
+          <h1  class="logo-text"><span>Coffee</span> Blog</h1>
+       
+        <p>
+          Coffe Blog is a fictional blog conceived for a school project.
+        </p>
+        <div class="contact">
+          
+          <span><i class="fas fa-envelope"></i> &nbsp; info@coffeeblog.com</span>
+        </div>
+        <div class="socials">
+          
+          <a href="https://www.instagram.com/ebkaraca/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+<!--       <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div> -->
+
+<!--       <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div> -->
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; coffeblog.com | Designed by Emre Baran Karaca
+    </div>
+  </div>
+  <!-- // footer -->
